fdae43cd201ead7488fc2d09d79b288a678c2b64
Alex Zidros <alexander.zidros@gmail.com>
2021-12-30 22:50:17 -0500

    first

---
+++ vial-qmk/keyboards/zmk4/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA4, 0xC3, 0x6A, 0x80, 0xA6, 0xBC, 0x1F, 0x90}
+
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
